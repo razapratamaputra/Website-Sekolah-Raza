0b936006cc8b812e61c3a1116a2fe8332c247432
razapratamaputra <razapratamaputra@gmail.com>
2025-06-15 15:58:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Mencegah pengiriman form
+    event.preventDefault();
 
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    // Tampilkan pesan sukses
     document.getElementById('responseMessage').innerText = `Terima kasih, ${name}! Kami akan menghubungi Anda di ${email}.`;
 
-    // Reset form
     this.reset();
 });
